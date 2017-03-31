a305cb7cd5ae07e3a5db968c303f09c77ccdfc24
Jerred Almasco <jtaalmasco@gmail.com>
2017-03-31 10:41:57 +0800

    Hello World

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 var express = require('express');
+var app = express();
+
+app.get('/',function(res,req){
+    res.send('Hello World')
+})
+
+app.listen(3000, function(){
+    console.log('Example app listening to port 3000!');
+})
